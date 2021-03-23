1274abf0c733a2c9c0b0241bb4038a31eb5958c0
Mikko Östlund <Mikko.Ostlund@netent.com>
2021-03-23 15:13:43 +0100

    Initial commit

---
+++ src/main/java/name/mikkoostlund/someconfigsvc/SomeConfigSvcApplication.java
@@ -0,0 +1,13 @@
+package name.mikkoostlund.someconfigsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeConfigSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeConfigSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/mikkoostlund/someconfigsvc/SomeConfigSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package name.mikkoostlund.someconfigsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeConfigSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
